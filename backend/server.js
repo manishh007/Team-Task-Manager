26ec30fd45d54ddaee6038fa0ee80dc8f402f9ae
Manish Pratap Singh <123986689+manishh007@users.noreply.github.com>
2026-05-03 19:58:26 +0530

    initial commit

--- backend/server.js
+++ backend/server.js
@@ -27,5 +27,9 @@ mongoose.connect(process.env.MONGO_URI)
     .then(() => console.log("MongoDB Connected"))
     .catch(err => console.log(err));
 
+
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on ${PORT}`));
+
+app.listen(PORT, () => {
+    console.log(`Server running on ${PORT}`);
+});
